0cc4552ffd4bb4a9b47dfa7f5c2b28ad6237508a
hukai1990 <hukai@smartens-tek.com>
2017-09-14 15:26:31 +0800

    add database

---
+++ CoolWeather/app/src/main/java/com/mycompany/coolweather/db/City.java
@@ -0,0 +1,47 @@
+package com.mycompany.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by sstek_mars on 2017/9/14.
+ */
+
+public class City extends DataSupport {
+
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int privinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getPrivinceId() {
+        return privinceId;
+    }
+
+    public void setPrivinceId(int privinceId) {
+        this.privinceId = privinceId;
+    }
+}
---
+++ CoolWeather/app/src/main/java/com/mycompany/coolweather/db/Province.java
@@ -0,0 +1,38 @@
+package com.mycompany.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by sstek_mars on 2017/9/14.
+ */
+
+public class Province extends DataSupport {
+
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    private int getId(){
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
